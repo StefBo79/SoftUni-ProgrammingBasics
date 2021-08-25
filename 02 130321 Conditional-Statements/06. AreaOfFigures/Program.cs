2fffb66bc3f4fcd95ff2b74e7fdcf8e19c680c15
StefBo <bonev@x-spot.org>
2021-08-26 00:03:16 +0300

    02 130321

---
+++ 02 130321 Conditional-Statements/06. AreaOfFigures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string shape = (Console.ReadLine());
+            double area = 0;
+            if (shape == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = side * side;
+            }
+
+            else if (shape == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                area = sideA * sideB;
+            }
+
+            else if (shape == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = (Math.PI * (radius * radius));
+            }
+
+            else if (shape == "triangle") 
+            {
+                double sideT = double.Parse(Console.ReadLine());
+                double hightT = double.Parse(Console.ReadLine());
+                area = sideT * hightT / 2;
+            }
+                Console.WriteLine($"{area:F3}");
+        }
+    }
+}
---
+++ 02 130321 Conditional-Statements/07. ToyShop/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // static prices
+
+            const double priceOfPuzzel = 2.60;
+            const double priceOfTalkingDolls = 3;
+            const double priceOfTeddyBear = 4.10;
+            const double priceOfMinion = 8.20;
+            const double priceOfTruck = 2;
+
+            // INPUT FROM THE USER:
+
+            double priceForVacantion = double.Parse(Console.ReadLine());
+            double countOfPuzzels = double.Parse(Console.ReadLine());
+            double countOfTalkingDolls = double.Parse(Console.ReadLine());
+            double countOfTeddyBears = double.Parse(Console.ReadLine());
+            double countOfMinions = double.Parse(Console.ReadLine());
+            double countOfTrucks = double.Parse(Console.ReadLine());
+
+            // LOGIC FOR SOLVING THE PROBLEM:
+
+            double countOfToys = countOfPuzzels + countOfTalkingDolls + countOfTeddyBears + countOfMinions + countOfTrucks;
+
+            double sum = countOfPuzzels * priceOfPuzzel + countOfTalkingDolls * priceOfTalkingDolls + countOfTeddyBears * priceOfTeddyBear + countOfMinions * priceOfMinion + countOfTrucks * priceOfTruck;
+
+            // OUT OF THE RESULT AFTER SOLVING THE PROBLEM:
+            double discount = 0;
+            if (countOfToys >= 50) 
+            {
+            discount = sum * 0.25;
+            }
+
+            double finalSum = sum - discount;
+            double rent = finalSum * 0.10;
+            double moneyMade = finalSum - rent;
+
+            if (priceForVacantion > moneyMade)
+            {
+                double moneyNeeded = priceForVacantion - moneyMade;
+                Console.WriteLine($"Not enough money! {moneyNeeded:F2} lv needed.");
+            }
+            else 
+            {
+                double moneyLeft = moneyMade - priceForVacantion;
+                Console.WriteLine($"Yes! {moneyLeft:F2} lv left.");
+            }
+        }
+    }
+}
