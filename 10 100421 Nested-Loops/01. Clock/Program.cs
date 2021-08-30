f0ae863aa8376593dfe4e0525752caf98f3e3b6b
StefBo <bonev@x-spot.org>
2021-08-30 19:47:13 +0300

    10 100421

---
+++ 10 100421 Nested-Loops/01. Clock/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Nested_Loops_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int h = 0; h <= 23; h++)
+            {
+                for (int m = 0; m <= 59; m++)
+                {
+                    for (int s = 0; s <= 59; s++)
+                    {
+                        Console.WriteLine($"{h:D2}:{m:D2}:{s:D2}");
+                    }
+                }
+            }
+        }
+    }
+}
