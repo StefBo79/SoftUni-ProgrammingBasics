e1e429e4e392ca737b789f1d58010ba0b392830e
Stefan Bonev <bonev@x-spot.org>
2022-03-27 12:47:53 +0300

    07 280321

--- 07 280321 ForLoop-Exercise/05. DivideWithoutRemainder/Program.cs
+++ 07 280321 ForLoop-Exercise/05. DivideWithoutRemainder/Program.cs
@@ -6,7 +6,31 @@ namespace DivideWithoutRemainder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    p1 += 1;
+                }
+                if (number % 3 == 0)
+                {
+                    p2 += 1;
+                }
+                if (number % 4 == 0)
+                {
+                    p3 += 1;
+                }
+            }
+
+            Console.WriteLine($"{p1 / n * 100:f2}%");
+            Console.WriteLine($"{p2 / n * 100:f2}%");
+            Console.WriteLine($"{p3 / n * 100:f2}%");
         }
     }
 }
