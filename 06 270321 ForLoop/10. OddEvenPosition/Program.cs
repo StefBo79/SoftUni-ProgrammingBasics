6588385437ff2d85586b67c81203bb592a52da71
Stefan Bonev <bonev@x-spot.org>
2022-03-27 12:12:31 +0300

    06 270321

--- 06 270321 ForLoop/10. OddEvenPosition/Program.cs
+++ 06 270321 ForLoop/10. OddEvenPosition/Program.cs
@@ -6,7 +6,34 @@ namespace OddEvenPosition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int oddPosition = 0;
+            int evenPosition = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenPosition += number;
+                }
+                else
+                {
+                    oddPosition += number;
+                }
+                
+            }
+
+            if (oddPosition == evenPosition)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {oddPosition}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(oddPosition - evenPosition)}");
+            }            
         }
     }
 }
