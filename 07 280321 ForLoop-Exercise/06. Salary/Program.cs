c3a8b60235b6d5841cee24450cdbdf1b4cc81d8f
Stefan Bonev <bonev@x-spot.org>
2022-03-27 19:59:37 +0300

    07 280321

--- 07 280321 ForLoop-Exercise/06. Salary/Program.cs
+++ 07 280321 ForLoop-Exercise/06. Salary/Program.cs
@@ -6,7 +6,43 @@ namespace Salary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string openTab = Console.ReadLine();
+
+                if (openTab == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if (openTab == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if (openTab == "Reddit")
+                {
+                    salary -= 50;
+                }
+                else if (openTab != "Facebook" || openTab != "Instagram" || openTab != "Redid")
+                {
+                    continue;
+                }
+                if (salary <= 0)
+                {                    
+                    break;
+                }
+            }
+
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);                
+            }
+            else
+            {
+                Console.WriteLine("You have lost your salary.");
+            }            
         }
     }
 }
